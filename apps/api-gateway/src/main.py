17a74d360b8ab35630681ff9b46e42a25d75554e
pratiksontakke <pratikass488@gmail.com>
2025-07-11 15:20:10 +0530

    api auth impl

--- apps/api-gateway/src/main.py
+++ apps/api-gateway/src/main.py
@@ -3,6 +3,7 @@ from supabase import create_client
 from .utils.config import settings
 from .middleware.auth_middleware import AuthMiddleware
 from .user.router import router as user_router
+from .auth.router import router as auth_router
 
 app = FastAPI(title=settings.APP_NAME)
 supabase_client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
@@ -11,6 +12,7 @@ app.add_middleware(AuthMiddleware, supabase_client=supabase_client)
 
 
 app.include_router(user_router, prefix="/api/v1")
+app.include_router(auth_router, prefix="/api/v1")
 
 
 @app.get("/")
